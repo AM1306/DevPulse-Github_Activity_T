e18a36f508f7246f2c817f315fb66a2d7fe624f3
Aadhya Mathur <aadhya.mathur13@gmail.com>
2026-06-14 17:45:04 +0530

    Added Fetching data from API

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../CSS/SearchPage.css";
+import Spinner from "../components/spinner";
 
 function SearchPage() {
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
-  const [isLoading,setIsLoading]=useState(false);
-  const [error,setError]=useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   function handleUsername(event) {
     setUsername(event.target.value);
   }
-  function handleSearch() {
-    navigate(`/user/${username}`);
+
+  async function handleSearch() {
+    if (!username.trim()) return;
+
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+
+      if (response.status === 404) {
+        setError("User not found.Please check the username.");
+        return;
+      }
+      navigate(`/user/${username}`);
+    } catch (error) {
+      setError("Something went wrong. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   }
-  
+
   return (
     <>
       <h1>SearchPage</h1>
@@ -32,6 +51,8 @@ function SearchPage() {
         ></input>
         <button onClick={handleSearch}>SEARCH PROFILE</button>
       </div>
+      {isLoading && <Spinner />}
+      {error && <p className="error">{error}</p>}
     </>
   );
 }
