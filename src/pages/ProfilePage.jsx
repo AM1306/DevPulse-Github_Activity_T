b9b12863090f25f373df8d15b300ffd35beb09fb
Aadhya Mathur <aadhya.mathur13@gmail.com>
2026-06-15 13:44:21 +0530

    fetching languages for each repo and is aggregate

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -10,30 +10,50 @@ function ProfilePage() {
   const { username } = useParams();
 
   useEffect(() => {
-    async function fetchProfileData(){
+    async function fetchProfileData() {
       setIsLoading(true);
       setError(null);
-      
-      try{
+
+      try {
         //Fetch profile
-        const profileRes=await fetch('https://api.github.com/users/{username}');
-        if(profileRes.status===404){
+        const profileRes = await fetch(
+          `https://api.github.com/users/${username}`,
+        );
+        if (profileRes.status === 404) {
           setError("User not found");
           return;
         }
-        const profileData=await profileRes.json();
+        const profileData = await profileRes.json();
         setProfile(profileData);
 
         //Fetch repos
-        const reposRes=await fetch(`https://api.github.com/users/${username}/repos?sort=stars`);
-        const reposData=await reposRes.json();
-        setRepo(reposDat.slice(0,6));
-      }catch(error){
-        setError("Something went wrong. Please try again later.",error);
-      }finally{
-        setIsLoading(false)
+        const reposRes = await fetch(
+          `https://api.github.com/users/${username}/repos?sort=stars`,
+        );
+        const reposData = await reposRes.json();
+        setRepo(reposData.slice(0, 6));
+
+        //fetch languages for each repo and aggregate
+        const top6 = reposData.slice(0, 6);
+        const langTotal ={};
+        for (const repo of top6) {
+          const langRes = await fetch(
+            `https://api.github.com/repos/${username}/${repo.name}/languages`,
+          );
+          const langData = await langRes.json();
+
+          for (const [lang, bytes] of Object.entries(langData)) {
+            langTotal[lang] = (langTotal[lang] || 0) + bytes;
+          }
+        }
+        setLanguages(langTotal);
+      } catch (error) {
+        setError("Something went wrong. Please try again later.", error);
+      } finally {
+        setIsLoading(false);
       }
     }
+    fetchProfileData();
   }, [username]);
 
   return (
