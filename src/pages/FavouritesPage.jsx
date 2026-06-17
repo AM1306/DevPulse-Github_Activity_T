dd46017216d32460f2bd5fb967f2af816af3b160
Aadhya Mathur <aadhya.mathur13@gmail.com>
2026-06-17 20:34:30 +0530

    Completed Favourites page

--- src/pages/FavouritesPage.jsx
+++ src/pages/FavouritesPage.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function FavouritesPage() {
+  const [favourite, setFavourite] = useState([]);
+
+  function removeFavourite(item) {
+    const updatedFav = favourite.filter((el) => el.login !== item.login);
+
+    setFavourite(updatedFav);
+
+    localStorage.setItem("favourites", JSON.stringify(updatedFav));
+  }
+
+  useEffect(() => {
+    const savedFav = JSON.parse(localStorage.getItem("favourites")) || [];
+    setFavourite(savedFav);
+  }, []);
+
   return (
     <>
-      <h1>FavouritesPage</h1>
+      <h1>Favourites Page</h1>
 
-      <Link to="/favourites">Favourites</Link>
+      {favourite.length === 0 ? (
+        <p>No favourites added yet.</p>
+      ) : (
+        favourite.map((item) => (
+          <div
+            key={item.login}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "12px",
+              marginBottom: "12px",
+            }}
+          >
+            <button onClick={() => removeFavourite(item)}>Remove</button>
+            <img
+              src={item.avatar_url}
+              alt={item.login}
+              width="50"
+              height="50"
+              style={{ borderRadius: "50%" }}
+            />
+
+            <Link to={`/user/${item.login}`}>{item.login}</Link>
+          </div>
+        ))
+      )}
     </>
   );
 }
